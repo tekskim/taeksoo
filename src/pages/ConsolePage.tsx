3a1d7d126c17c79b36f6d3f0e64046af5ce52bee
remy <pobae.kim@thakicloud.co.kr>
2026-01-06 12:46:16 +0900

    fix: 모든 페이지에서 useSidebar 훅 사용하여 사이드바 상태 동기화

--- src/pages/ConsolePage.tsx
+++ src/pages/ConsolePage.tsx
@@ -161,7 +161,7 @@ export function ConsolePage() {
       
       <main
         className={`absolute top-0 bottom-0 right-0 flex flex-col bg-[var(--color-surface-default)] transition-[left] duration-200 ${
-          sidebarOpen ? 'left-[200px]' : 'left-[48px]'
+          sidebarOpen ? 'left-[200px]' : 'left-0'
         }`}
       >
         {/* Fixed Header Area */}
