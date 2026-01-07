671192396aa7197a1334e4702b868866a3642f07
taeksoo.kim <taeksoo.kim@taeksookimui-MacBookPro.local>
2026-01-07 13:02:42 +0900

    chore: Remove thaki-cloud directory from main branch
    - Desktop functionality (DesktopPage, SettingsPage, ChatbotPanel) is already in src/ directory
    - thaki-cloud directory is not needed in main branch structure

--- thaki-cloud/src/design-system/components/Disclosure/index.ts
+++
@@ -1,7 +0,0 @@
-export { Disclosure, DisclosureTrigger, DisclosurePanel } from './Disclosure';
-export type { DisclosureProps, DisclosureTriggerProps, DisclosurePanelProps } from './Disclosure';
-
-
-
-
-
--- thaki-cloud/src/design-system/components/Slider/index.ts
+++
@@ -1,7 +0,0 @@
-export { Slider } from './Slider';
-export type { SliderProps } from './Slider';
-
-
-
-
-
