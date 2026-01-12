24b4bfe0770db44406e0e79d29896791ac267985
remy <pobae.kim@thakicloud.co.kr>
2026-01-12 15:27:19 +0900

    fix: ComputeImagesPage useNavigate 중복 import 제거

--- src/pages/ComputeImagesPage.tsx
+++ src/pages/ComputeImagesPage.tsx
@@ -29,7 +29,7 @@ import {
   IconDownload,
   IconBell,
 } from '@tabler/icons-react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 /* ----------------------------------------
    Types
