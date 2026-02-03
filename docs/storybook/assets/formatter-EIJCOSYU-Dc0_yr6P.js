b4043bbe34205d8dde52b6a5795f515702148d54
remy <pobae.kim@thakicloud.co.kr>
2026-02-03 15:34:16 +0900

    chore: docs 빌드 업데이트 (label-lg line-height 18px)

--- docs/storybook/assets/formatter-EIJCOSYU-Dc0_yr6P.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as o}from"./DocsRenderer-LL677BLK-DmgCrRo4.js";import{I as t}from"./iframe-BIZW5Rzi.js";import"./preload-helper-C1FmrZbK.js";import"./react-18-B7YFAsbG.js";import"./index-nzsV0QN0.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
