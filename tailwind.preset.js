d8e2888130ee7f2598e5607d13d17769c768f2a2
remy <pobae.kim@thakicloud.co.kr>
2026-02-06 08:31:13 +0900

    fix: change star icon color from warning(orange) to yellow400

--- tailwind.preset.js
+++ tailwind.preset.js
@@ -1,5 +1,5 @@
 // Auto-generated from tokens/light.json - DO NOT EDIT
-// Generated at: 2026-02-05T06:53:53.091Z
+// Generated at: 2026-02-05T06:51:53.240Z
 
 /** @type {import('tailwindcss').Config} */
 export default {
